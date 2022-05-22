914e6b499604e9062de5aa2263a69b3dfa53c0d0
Damian Skuraś <91007341+Dasker90@users.noreply.github.com>
2022-05-22 22:05:45 +0200

    Zadanie 1.1
    Task 1.1
    Counting the area of ​​a rectangle

---
+++ Zadanie 1.1.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Zadanie_11
+{
+    class Program
+    {
+        static Void Main (string[] args)
+        {
+            double a, b, pole;
+
+            Console.WriteLine("A.H.E | Informatyka | III rok");
+            Console.WriteLine("Damian Skuraś");
+            Console.WriteLine("Zadanie 1.1");
+
+            Console.WriteLine("Program oblicza pole prostokata :");
+            Console.WriteLine("Podaj bok a:");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj bok b:");
+            b = double.Parse(Console.ReadLine());
+
+            pole = a * b;
+
+            Console.WriteLine("Pole prostokąta o boku a = {0} i boku b = {1}, a , b");
+            Console.WriteLine("Wynosi {0}.",pole);
+
+        }
+    }
+}
