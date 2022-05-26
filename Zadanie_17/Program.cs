b21accf3989a5202b8c86b8234bb4bcf28ff19d1
Damian Skuraś <91007341+Dasker90@users.noreply.github.com>
2022-05-26 19:01:46 +0200

    Add files via upload

---
+++ Zadanie_17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Zadanie_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, suma, roznica, iloczyn, iloraz;
+
+            Console.WriteLine("Podaj X:");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj Y:");
+            y = double.Parse(Console.ReadLine());
+
+            suma = x + y;
+            roznica = x - y;
+            iloczyn = x * y;
+            iloraz = x / y;
+
+            Console.WriteLine("Dla x = {0:##.##} i y = {1:##.##}", x, y);
+            Console.WriteLine("suma = {0:##.##}|", suma);
+            Console.WriteLine("roznica = {0:##.##}|", roznica);
+            Console.WriteLine("iloraz = {0:##.##}|", iloraz);
+            Console.WriteLine("iloczyn = {0:##.##}|", iloczyn);
+        }
+    }
+}
---
+++ Zadanie_21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zadanie_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Z liczb a b c czy tworzą one trójke pitagorejska:");
+            Console.WriteLine("Podaj a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj c:");
+            c = int.Parse(Console.ReadLine());
+
+            if ((a*a+b*b)== c*c)
+            {
+                Console.Write("Liczby");
+                Console.Write(" a = "+a+".");
+                Console.Write(" b = "+b+".");
+                Console.Write(" c = "+c);
+                Console.WriteLine(" są trójką pitagorejską. ");
+            }
+            else
+            {
+                Console.Write("Liczby");
+                Console.Write(" a = " + a + ".");
+                Console.Write(" b = " + b + ".");
+                Console.Write(" c = " + c);
+                Console.WriteLine(" nie są trójką pitagorejską. ");
+            }
+        }
+    }
+}
