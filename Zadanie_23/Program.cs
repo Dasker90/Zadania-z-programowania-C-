47103937bde57a9c15ac764ba403096d603babce
Damian Skuraś <91007341+Dasker90@users.noreply.github.com>
2022-05-26 19:06:10 +0200

    Add files via upload

---
+++ Zadanie_23/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using static System.Math;
+
+namespace Zadanie_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, x1, x2;
+            byte liczba_pierwiastkow = 0;
+
+            Console.WriteLine("Program ma liczyc pierwiastki rownania kwadratowego ax^2 + bx + c = 0 ");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Podaj a. ");
+            a = double.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                Console.WriteLine("Niedozwolona wartosc wspolczynnika a.");
+                Console.WriteLine("--------------------------------------------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("--------------------------------------------------------------------------");
+                Console.WriteLine("Podaj b. ");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine("--------------------------------------------------------------------------");
+                Console.WriteLine("Podaj c. ");
+                c = double.Parse(Console.ReadLine());
+                Console.WriteLine("--------------------------------------------------------------------------");
+
+                delta = b * b - 4 * a * c;
+
+                if (delta < 0) liczba_pierwiastkow = 0;
+                if (delta == 0) liczba_pierwiastkow = 1;
+                if (delta > 0) liczba_pierwiastkow = 2;
+
+                switch (liczba_pierwiastkow)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine();
+                            Console.Write("Dla ");
+                            Console.Write(" a = {0} .", a);
+                            Console.Write(" b = {0} .", b);
+                            Console.Write(" c = {0} .", c);
+                            Console.Write(" brak pierwiastków rzeczywistych. ");
+                            Console.WriteLine("--------------------------------------------------------------------------");
+                        }
+                        break;
+                    case 1:
+                        {
+                            x1 = -b / (2 * a);
+                            Console.WriteLine();
+                            Console.Write("Dla ");
+                            Console.Write(" a = {0} .", a);
+                            Console.Write(" b = {0} .", b);
+                            Console.Write(" c = {0} .", c);
+                            Console.WriteLine("trojmian ma jeden pierwiastek podwojny x1 = {0:##.##}.", x1);
+                            Console.WriteLine("--------------------------------------------------------------------------");
+                        }
+                        break;
+                    case 2:
+                        {
+                            x1 = (-b - Sqrt(delta)) / (2 * a);
+                            x2 = (-b + Sqrt(delta)) / (2 * a);
+                            Console.WriteLine();
+                            Console.Write("Dla ");
+                            Console.Write(" a = {0} .", a);
+                            Console.Write(" b = {0} .", b);
+                            Console.Write(" c = {0} .", c);
+                            Console.WriteLine("trojmian ma dwa pierwiastki x1 = {0:##.##}.  x2 = {1:##.##}. ", x1, x2);
+                            Console.WriteLine("--------------------------------------------------------------------------");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Zadanie_24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zadanie_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, x;
+            Console.WriteLine(" Program oblicza wartosc x z rownania liniowego ax+b=0  ");
+            Console.WriteLine(" Podaj a. ");
+            a = double.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console.WriteLine(" Niedozwolona wartosc wspołczynnika a: !!!!!");
+            }
+            else
+            {
+                Console.WriteLine(" Podaj b. ");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine(" Podaj a. ");
+                c = double.Parse(Console.ReadLine());
+
+                x = (c - b) / a;
+
+                Console.WriteLine();
+                Console.WriteLine("Dla");
+                Console.Write(" a = {0:##.##}, ", a);
+                Console.Write(" b = {0:##.##}, ", b);
+                Console.Write(" c = {0:##.##}, ", c);
+                Console.WriteLine(" wartosc x = {0:##.##}. ",x);
+            }
+        }
+    }
+}
