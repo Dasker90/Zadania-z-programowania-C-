cfcea0727f9d547239f1a3017f6f2d15512528bb
Damian Skuraś <91007341+Dasker90@users.noreply.github.com>
2022-05-26 19:19:58 +0200

    Add files via upload

---
+++ Zadanie_322/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Zadanie_322
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char znak;
+
+
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("Program wyswietla duze litery alfabetu od A do Z i od Z do A ");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------------");
+
+            for (znak = 'A'; znak <= 'Z'; znak++)
+            {
+                if (znak < 'Z')
+                    Console.Write(znak + ".");
+                else
+                {
+                    Console.Write(znak + ".");
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+
+            for (znak = 'Z'; znak >= 'A'; znak--)
+            {
+                if (znak > 'A')
+                    Console.Write(znak + ".");
+                else
+                {
+                    Console.Write(znak + ".");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
